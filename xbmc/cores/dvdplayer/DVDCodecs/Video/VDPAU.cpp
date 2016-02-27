0384faa5225adfc7004d176990e33277762459f2
Martin Ostertag <martin.ostertag@gmx.de>
2016-02-27 12:36:38 +0100

    fix format detection issue due to clash of VDP enum value and ReadFormatOf return value

--- xbmc/cores/dvdplayer/DVDCodecs/Video/VDPAU.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/VDPAU.cpp
@@ -611,8 +611,8 @@ bool CDecoder::Open(AVCodecContext* avctx, const enum PixelFormat fmt, unsigned 
     VdpDecoderProfile profile = 0;
 
     // convert FFMPEG codec ID to VDPAU profile.
-    ReadFormatOf(avctx->codec_id, profile, m_vdpauConfig.vdpChromaType);
-    if(profile != -1)
+    bool found = ReadFormatOf(avctx->codec_id, profile, m_vdpauConfig.vdpChromaType);
+    if(found)
     {
       VdpStatus vdp_st;
       VdpBool is_supported = false;
@@ -934,10 +934,12 @@ void CDecoder::FiniVDPAUOutput()
   m_videoSurfaces.Reset();
 }
 
-void CDecoder::ReadFormatOf( AVCodecID codec
+bool CDecoder::ReadFormatOf( AVCodecID codec
                            , VdpDecoderProfile &vdp_decoder_profile
                            , VdpChromaType     &vdp_chroma_type)
 {
+  bool found = true;
+  
   switch (codec)
   {
     case AV_CODEC_ID_MPEG1VIDEO:
@@ -969,10 +971,12 @@ void CDecoder::ReadFormatOf( AVCodecID codec
       vdp_chroma_type     = VDP_CHROMA_TYPE_420;
       break;
     default:
-      vdp_decoder_profile = -1;
-      vdp_chroma_type     = -1;
+      vdp_decoder_profile = 0;
+      vdp_chroma_type     = 0;
+      found = false;
       break;
   }
+  return found;
 }
 
 bool CDecoder::ConfigVDPAU(AVCodecContext* avctx, int ref_frames)
