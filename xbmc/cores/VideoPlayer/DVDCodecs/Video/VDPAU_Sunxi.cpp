bd4eacf0cd2a2eb6c8646861ad5c50fb5e450783
Martin Ostertag <Martin.Ostertag@gmx.de>
2020-06-21 15:25:38 +0200

    Allwinner: removed unnecessary allocation of AVFrame in VDAPU_SUNXI

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/VDPAU_Sunxi.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/VDPAU_Sunxi.cpp
@@ -245,7 +245,7 @@ void CVDPAUContext::SpewHardwareAvailable()  //Copyright (c) 2008 Wladimir J. va
 //-----------------------------------------------------------------------------
 
 CDecoder::CDecoder(CProcessInfo& processInfo) :
-    m_processInfo(processInfo)
+  m_processInfo(processInfo)
 {
   m_vdpauConfigured = false;
   m_DisplayState = VDPAU_OPEN;
@@ -422,7 +422,7 @@ long CDecoder::Release()
     {
       auto elem = m_decodedPics.front();
       m_decodedPics.pop();
-      av_frame_unref(elem);
+      delete elem;
     }
   }
   return IHardwareDecoder::Release();
@@ -738,9 +738,13 @@ CDVDVideoCodec::VCReturn CDecoder::Decode(AVCodecContext *avctx, AVFrame *pFrame
 
   if(pFrame)
   { // we have a new frame from decoder
-    AVFrame *pic = av_frame_alloc();
-    av_frame_move_ref(pic, pFrame);
-    m_decodedPics.push(pic);
+    CVideoBufferSunxi* buffer = dynamic_cast<CVideoBufferSunxi*>(m_videoBufferPool->Get());
+    assert(buffer);
+    buffer->SetRef(pFrame);
+    
+/*    AVFrame *pic = av_frame_alloc();
+    av_frame_move_ref(pic, pFrame);*/
+    m_decodedPics.push(buffer);
 
     m_bufferStats.IncDecoded();
   }
@@ -782,19 +786,19 @@ bool CDecoder::GetPicture(AVCodecContext* avctx, VideoPicture* picture)
   if (m_DisplayState != VDPAU_OPEN)
     return false;
 
-  if(m_pFrame)
-    av_frame_free(&m_pFrame);
+  //if(m_pFrame)
+  //  av_frame_free(&m_pFrame);
 
-  m_pFrame = m_decodedPics.front();
+  CVideoBufferSunxi* buffer = m_decodedPics.front();
   m_decodedPics.pop();
   m_bufferStats.DecDecoded();
 
-  SetPictureParams(picture);
+  buffer->SetPictureParams(picture);
 
   //std::shared_ptr<CVideoBufferPoolSunxi> pool = ((CProcessInfoSunxi&)(m_processInfo)).GetBufferPool();
-  CVideoBufferSunxi* buffer = dynamic_cast<CVideoBufferSunxi*>(m_videoBufferPool->Get());
-  assert(buffer);
-  buffer->SetRef(m_pFrame);
+//   CVideoBufferSunxi* buffer = dynamic_cast<CVideoBufferSunxi*>(m_videoBufferPool->Get());
+//   assert(buffer);
+//   buffer->SetRef(m_pFrame);
 
   picture->videoBuffer = buffer;
   return true;
@@ -812,7 +816,7 @@ void CDecoder::Reset()
   {
     auto elem = m_decodedPics.front();
     m_decodedPics.pop();
-    av_frame_unref(elem);
+    delete elem;
   }
   m_bufferStats.Reset();
 }
@@ -878,43 +882,4 @@ void CDecoder::Register()
     settings->GetSetting(CSettings::SETTING_VIDEOPLAYER_USEVDPAUMPEG2)->SetVisible(true);
     settings->GetSetting(CSettings::SETTING_VIDEOPLAYER_USEVDPAUMIXER)->SetVisible(true);
   }
-}
-
-void CDecoder::SetPictureParams(VideoPicture* pVideoPicture)
-{
-  pVideoPicture->iWidth = m_pFrame->width;
-  pVideoPicture->iHeight = m_pFrame->height;
-
-  double aspect_ratio = 0;
-  AVRational pixel_aspect = m_pFrame->sample_aspect_ratio;
-  if (pixel_aspect.num)
-    aspect_ratio = av_q2d(pixel_aspect) * pVideoPicture->iWidth / pVideoPicture->iHeight;
-
-  if (aspect_ratio <= 0.0)
-    aspect_ratio = (float)pVideoPicture->iWidth / (float)pVideoPicture->iHeight;
-
-  pVideoPicture->iDisplayWidth = ((int)lrint(pVideoPicture->iHeight * aspect_ratio)) & -3;
-  pVideoPicture->iDisplayHeight = pVideoPicture->iHeight;
-  if (pVideoPicture->iDisplayWidth > pVideoPicture->iWidth)
-  {
-    pVideoPicture->iDisplayWidth = pVideoPicture->iWidth;
-    pVideoPicture->iDisplayHeight = ((int)lrint(pVideoPicture->iWidth / aspect_ratio)) & -3;
-  }
-
-  pVideoPicture->color_range = m_pFrame->color_range == AVCOL_RANGE_JPEG ? 1 : 0;
-  pVideoPicture->color_primaries = m_pFrame->color_primaries;
-  pVideoPicture->color_transfer = m_pFrame->color_trc;
-  pVideoPicture->color_space = m_pFrame->colorspace;
-
-  pVideoPicture->iRepeatPicture = 0;
-  pVideoPicture->iFlags = 0;
-  pVideoPicture->iFlags |= m_pFrame->interlaced_frame ? DVP_FLAG_INTERLACED : 0;
-  pVideoPicture->iFlags |= m_pFrame->top_field_first ? DVP_FLAG_TOP_FIELD_FIRST: 0;
-  pVideoPicture->iFlags |= m_pFrame->data[0] ? 0 : DVP_FLAG_DROPPED;
-
-  int64_t pts = m_pFrame->pts;
-  if (pts == AV_NOPTS_VALUE)
-    pts = m_pFrame->best_effort_timestamp;
-  pVideoPicture->pts = (pts == AV_NOPTS_VALUE) ? DVD_NOPTS_VALUE : (double)pts * DVD_TIME_BASE / AV_TIME_BASE;
-  pVideoPicture->dts = DVD_NOPTS_VALUE;
 }
